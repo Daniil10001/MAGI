8d09e6d9d7089179c59f3fa76dcb63379e90c09a
Daniil10001 <daniiltimohin8@gmail.com>
2025-11-14 03:20:25 +0300

    UART now is working!

--- scripts/SPIsrv.py
+++ scripts/SPIsrv.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
-from magi.srv import SPIList, SPICrt, SPIUnreg
+from magi.srv import SPIList, SPICrt, SPIUnreg,SPIdrqst
 from magi.msg import SPIInstance
 import spidev
+from threading import Lock
 
 import rclpy
 from rclpy.executors import ExternalShutdownException
@@ -18,53 +19,68 @@ class SPIsrv(Node):
         self.crt_srv = self.create_service(SPICrt, 'SPICreateInstance', self.createInstance)
         self.unreg_srv= self.create_service(SPIUnreg, 'SPIUnregInstance', self.unregisterIstance)
         self.list_srv= self.create_service(SPIList, 'SPIListInstances', self.listInterfces)
+        self.trs_srv= self.create_service(SPIdrqst, 'SPITransaction', self.transaction)
+        self.lk=Lock()
 
     def createInstance(self, request, response):
-        if len(self.availids)==0:
-            response.result=False
-            return response
-        response.inst_num=self.availids.pop()
-        try:
-            spi=None#spidev.SpiDev()
-            #spi.open(request.bus_num,request.device_num)
-            #spi.max_speed_hz = request.speed  # 1MHz
-            #spi.mode = request.mode
-            self.interfaces[response.inst_num]=(request.bus_num,request.device_num, spi)
-            response.result=True
-        except Exception as e:
-            print(e)
-            self.availids.append(response.inst_num)
-            response.result=False
-            return response
+        with self.lk:
+            if len(self.availids)==0:
+               response.result=False
+               return response
+            response.inst_num=self.availids.pop()
+            try:
+                spi=None#spidev.SpiDev()
+                #spi.open(request.bus_num,request.device_num)
+                #spi.max_speed_hz = request.speed  # 1MHz
+                #spi.mode = request.mode
+                self.interfaces[response.inst_num]=(request.name,request.bus_num,\
+                                                   request.device_num, spi, Lock())
+                response.result=True
+            except Exception as e:
+                print(e)
+                self.availids.append(response.inst_num)
+                response.result=False
+                return response
         return response
     
     def unregisterIstance(self, request, response):
-        if request.inst_num in self.interfaces.keys():
-            try:
-                self.interfaces[response.inst_num][2].close()
-            except:
-                pass #do some logs
-            self.interfaces.pop(request.inst_num)
-            self.availids.append(request.inst_num)
-            self.availids.sort(reverse=True)
-            response.result=True
-        else:
-            response.result=False
+        with self.lk:
+            if request.inst_num in self.interfaces.keys():
+                with self.interfaces[response.inst_num][4]:
+                    try:
+                        self.interfaces[response.inst_num][3].close()
+                    except:
+                        pass #do some logs
+                    self.interfaces.pop(request.inst_num)
+                    self.availids.append(request.inst_num)
+                    self.availids.sort(reverse=True)
+                    response.result=True
+            else:
+                response.result=False
         return response
     
     def listInterfces(self, request, response):
-        for k in self.interfaces:
-            intf=SPIInstance()
-            intf.inst_num=k
-            intf.interface_num,intf.device_num,_=self.interfaces[k]
-            response.instances.append(intf)
+        if self.lk:
+            for k in self.interfaces:
+                intf=SPIInstance()
+                intf.inst_num=k
+                intf.name,intf.interface_num,intf.device_num,_,_=self.interfaces[k]
+                response.instances.append(intf)
         response.num=len(response.instances)
+        return response
+
+    def transaction(self, request, response):
+        if request.inst_num in self.interfaces.keys():
+            with self.interfaces[response.inst_num][4]:
+                response.data_r=self.interfaces[response.inst_num][3].xfer(request.data_t)
+        else:
+            response.result=False
         return response
 
     def __del__(self):
         for k in self.interfaces:
             try:
-                self.interfaces[k][2].close()
+                self.interfaces[k][3].close()
             except:
                 pass #do some logs
 
---
+++ scripts/UARTsrv.py
@@ -0,0 +1,126 @@
+#!/usr/bin/env python3
+from magi.srv import UARTList, UARTCrt, UARTUnreg
+from magi.msg import UARTInstance
+from std_msgs.msg import UInt8
+import serial
+from threading import Lock
+
+import time
+import rclpy
+import threading
+from rclpy.executors import ExternalShutdownException
+from rclpy.node import Node
+from rclpy.executors import MultiThreadedExecutor
+
+
+
+class UARTPortHandler(Node):
+
+    def __init__(self,port:str,baudrate:int,inst_num:int):
+        super().__init__(f"uart_{inst_num}")
+        self.interface=serial.Serial(port,baudrate,timeout=0)
+        self.inst_num=inst_num
+        self.publisher=self.create_publisher(UInt8, f"uart_{inst_num}_recieve", 4096*2)
+        self.listener=self.create_subscription(UInt8, f"uart_{inst_num}_transmit", self._transmit,4096*2)
+        self.running=True
+        print(f"uart_{inst_num} inited")
+        self.read_thread = threading.Thread(target=self._read)
+        self.read_thread.start()
+
+
+    def _transmit(self,msg):
+        self.interface.write(msg.data.to_bytes(1))
+        pass
+
+    def _read(self):
+        while self.running:
+            try:
+                data = self.interface.read(1000)
+                if data:
+                    for d in data:
+                        self.publisher.publish(UInt8(data=d))
+            except serial.SerialException as e:
+                    print(f"Serial error: {e}")
+                    self.running = False
+            time.sleep(1/100)
+
+    def __del__(self):
+        self.running=False
+
+
+class UARTsrv(Node):
+
+    availids=[i for i in range(200)][::-1]
+    interfaces=dict()
+
+    def __init__(self,Executor):
+        super().__init__('UARThandler')
+        self.Executor=Executor
+        self.crt_srv = self.create_service(UARTCrt, 'UARTCreateInstance', self.createInstance)
+        self.unreg_srv= self.create_service(UARTUnreg, 'UARTUnregInstance', self.unregisterIstance)
+        self.list_srv= self.create_service(UARTList, 'UARTListInstances', self.listInterfces)
+        self.lk=Lock()
+    
+    def createInstance(self, request, response):
+        with self.lk:
+            if len(self.availids)==0:
+               response.result=False
+               return response
+            response.inst_num=self.availids.pop()
+            try:
+                UARTI=UARTPortHandler(request.port,request.baudrate,\
+                                    response.inst_num)
+                self.Executor.add_node(UARTI)
+                self.interfaces[response.inst_num]=(request.name,request.port,\
+                                                    request.baudrate,\
+                                                    UARTI, Lock())
+                response.result=True
+            except Exception as e:
+                print(e)
+                self.availids.append(response.inst_num)
+                response.result=False
+                return response
+        return response
+    
+    def unregisterIstance(self, request, response):
+        with self.lk:
+            if request.inst_num in self.interfaces.keys():
+                with self.interfaces[response.inst_num][4]:
+                    try:
+                        self.Executor.remove_node(self.interfaces[response.inst_num][3])
+                        self.interfaces[response.inst_num][3].destroy_node()
+                    except:
+                        pass #do some logs
+                    self.interfaces.pop(request.inst_num)
+                    self.availids.append(request.inst_num)
+                    self.availids.sort(reverse=True)
+                    response.result=True
+            else:
+                response.result=False
+        return response
+    
+    def listInterfces(self, request, response):
+        if self.lk:
+            for k in self.interfaces:
+                intf=UARTInstance()
+                intf.inst_num=k
+                intf.name,intf.interface_num,intf.device_num,_,_=self.interfaces[k]
+                response.instances.append(intf)
+        response.num=len(response.instances)
+        return response
+
+
+def main():
+    try:
+        with rclpy.init():
+            Executor=MultiThreadedExecutor()
+            srv = UARTsrv(Executor)
+            Executor.add_node(srv)
+            #rclpy.spin(srv)
+            Executor.spin()
+    except (KeyboardInterrupt, ExternalShutdownException):
+        pass
+
+
+if __name__ == '__main__':
+    main()
